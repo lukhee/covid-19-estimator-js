0e0c606ea4f28d8c26695ec3e7765ad567b38a0d
balogun lukman <o.balogun@ymail.com>
2020-04-09 01:46:42 +0100

    return sttatement error

--- src/estimator.js
+++ src/estimator.js
@@ -1,6 +1,7 @@
 const covid19ImpactEstimator = (data) => {
+  const input = data;
   return {
-    data,
+    data: input,
     impact: {
       currentlyInfected: data.reportedCase * 10,
       infectionsByRequestedTime: data.reportedCase * 10 * 1024
