be004a94605d2268549daede591fda943b56d450
balogun lukman <o.balogun@ymail.com>
2020-04-09 01:38:23 +0100

    proper indentation

--- src/estimator.js
+++ src/estimator.js
@@ -1,15 +1,15 @@
 const covid19ImpactEstimator = (data) => {
-    return {
-        data,
-        impact: {
-            currentlyInfected: data.reportedCase * 10,
-            infectionsByRequestedTime: data.reportedCase * 10 * 1024
-        },
-        severeImpact: {
-            currentlyInfected: data.reportedCase * 50,
-            infectionsByRequestedTime: data.reportedCase * 50 * 1024
-        }
-    };
+  return {
+    data,
+    impact: {
+      currentlyInfected: data.reportedCase * 10,
+      infectionsByRequestedTime: data.reportedCase * 10 * 1024
+    },
+    severeImpact: {
+      currentlyInfected: data.reportedCase * 50,
+      infectionsByRequestedTime: data.reportedCase * 50 * 1024
+    }
+  };
 }
 
 export default covid19ImpactEstimator;
